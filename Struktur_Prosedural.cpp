2d1f461236e50cb5e22929d3a9497f091374f704
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-03-31 17:58:52 +0800

    SELESAI

--- Struktur_Prosedural.cpp
+++ Struktur_Prosedural.cpp
@@ -15,12 +15,12 @@ bool cekPrima(int n) {
     }
     return true;
 }
+
 // Function untuk mengecek apakah angka termasuk Deret Fibonacci
 bool cekFibonacci(int n) {
     if (n < 0) return false;
     int a = 0;
     int b = 1;
-
     while (a < n) {
         int temp = a + b;
         a = b;
@@ -28,12 +28,14 @@ bool cekFibonacci(int n) {
     }
     return (a == n);
 }
+
 // Prosedur untuk menerima input angka dari pengguna
 void inputAngka() {
     cout << "Masukkan angka yang ingin dicek: ";
     cin >> angka;
 }
-// Prosedur untuk menampilkan menu pilihan
+
+// Prosedur untuk menampilkan menu
 void tampilkanMenu() {
     cout << "\n===============================" << endl;
     cout << "      MENU PENGECEKAN ANGKA    " << endl;
@@ -44,6 +46,7 @@ void tampilkanMenu() {
     cout << "Pilih menu (0-2): ";
     cin >> pilihan;
 }
+
 // Prosedur untuk menampilkan hasil Prima
 void tampilkanHasilPrima() {
     if (cekPrima(angka)) {
@@ -58,14 +61,15 @@ void tampilkanHasilFibonacci() {
     if (cekFibonacci(angka)) {
         cout << "=> " << angka << " termasuk dalam DERET FIBONACCI." << endl;
     } else {
-        cout << "=> " << angka << " TIDAK termasuk dalam deret Fibonacci." << endl;
+        cout << "=> " << angka << " TIDAK termasuk deret Fibonacci." << endl;
     }
 }
-// Fungsi utama
+
 int main() {
-// Menu utama menggunakan while loop agar terus berjalan
-while (true) {
+    // Menu utama menggunakan while loop agar terus berjalan
+    while (true) {
         tampilkanMenu();
+
         switch (pilihan) {
             case 1:
                 inputAngka();
